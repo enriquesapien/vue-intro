76eaf1e5157c21fa1c1fb3c141d2aafaf87e89c1
Enrique Sapien <esapien@hotmail.com>
2018-09-10 02:25:18 -0500

    Add links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/blog">Blog</router-link>
+      <router-link to="/account">Account</router-link>
+    </div>
     <router-view></router-view>
     <!-- <app-user username="Enrique"></app-user> -->
-    
+
   </div>
 </template>
 
