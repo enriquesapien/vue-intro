ab710cac0ce89251dc6fe5523f6d1aa59e853bf7
Enrique Sapien <esapien@hotmail.com>
2018-09-10 01:18:41 -0500

    Add custom Vue component

---
+++ src/components/User.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="user">
+        <h3>The User</h3>
+
+        <p>My username is {{ username }}</p>
+
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+    name: 'user',
+    props: {
+        username: String
+    }
+})
+</script>
+
+<style>
+#user {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import User from './components/User'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// To use globally in the app
+// Vue.component('app-user', User);
 
 new Vue({
   render: h => h(App)
